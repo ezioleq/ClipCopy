88a6d292426092f7b188940011267da80ddcf081
Ezioleq <git@ezioleq.com>
2023-08-18 20:43:32 +0200

    Add EF Core to the project
    - Add DatabaseContext

---
+++ ClipCopy/DatabaseContext.cs
@@ -0,0 +1,38 @@
+using ClipCopy.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClipCopy;
+
+/// <summary>
+/// Application database context.
+/// </summary>
+public class DatabaseContext : DbContext
+{
+    /// <summary>
+    /// A set of <see cref="ClipEntry"/>.
+    /// </summary>
+    public DbSet<ClipEntry> ClipEntries { get; set; } = null!;
+
+    private string DatabasePath { get; }
+    private const string DatabaseFilename = "database.db";
+
+    /// <summary>
+    /// Default <see cref="DatabaseContext"/> constructor.
+    /// </summary>
+    /// <exception cref="IOException">Failed to get application's data directory.</exception>
+    public DatabaseContext()
+    {
+        var path = Application.Context.FilesDir?.Path;
+
+        if (string.IsNullOrEmpty(path))
+            // This SHOULDN'T happen.
+            throw new IOException("Failed to get Data Files Directory path");
+
+        DatabasePath = Path.Join(DatabasePath, DatabaseFilename);
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite($"Filename={DatabasePath}");
+    }
+}
