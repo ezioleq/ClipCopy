07cad6b2ad1086dc4f58f28b6de2c869046598fe
Ezioleq <git@ezioleq.com>
2023-08-18 18:39:34 +0200

    Refactor ReceiveTextActivity to be more modular and error proof

--- ClipCopy/Activities/ReceiveTextActivity.cs
+++ ClipCopy/Activities/ReceiveTextActivity.cs
@@ -1,4 +1,6 @@
 using Android.Content;
+using Android.Util;
+using ClipCopy.Resources;
 
 namespace ClipCopy.Activities;
 
@@ -23,21 +25,92 @@ namespace ClipCopy.Activities;
 )]
 public class ReceiveTextActivity : Activity
 {
+    private const string Tag = nameof(ReceiveTextActivity);
+    private const string ClipboardLabel = "ClipCopy";
+
     protected override void OnStart()
     {
         base.OnStart();
 
+        HandleIncomingIntent();
+        Finish();
+    }
+
+    /// <summary>
+    /// Handle the caller's incoming intent.
+    /// </summary>
+    private void HandleIncomingIntent()
+    {
         if (Intent?.Action != Intent.ActionSend)
             return;
 
-        var text = Intent?.GetStringExtra(Intent.ExtraText) ?? "NOTHING";
+        var receivedText = GetReceivedIntentText();
+        var clipboardSet = SetClipboardContent(receivedText);
 
-        var clipboardService = (ClipboardManager)Application.Context.GetSystemService(ClipboardService)!;
-        var clipData = ClipData.NewPlainText("clip", text);
+        if (!clipboardSet)
+            return;
+
+        var toastMessage = string.Format(Localization.clipboard_copied, receivedText);
+        ShowToast(toastMessage);
+    }
+
+    /// <summary>
+    /// Get <see cref="Intent.ExtraText"/> value from the caller intent.
+    /// </summary>
+    /// <returns>Received text on success or <see cref="string.Empty"/> otherwise.</returns>
+    private string GetReceivedIntentText()
+    {
+        if (Intent is null)
+            return string.Empty;
+
+        var text = Intent.GetStringExtra(Intent.ExtraText);
+
+        if (string.IsNullOrEmpty(text))
+            return string.Empty;
+
+        return text;
+    }
+
+    /// <summary>
+    /// Sets the clipboard content to the provided text.
+    /// </summary>
+    /// <param name="content">Text content to set as the primary clip content.</param>
+    /// <returns>Whether the operation finished successfully.</returns>
+    private bool SetClipboardContent(string content)
+    {
+        if (string.IsNullOrEmpty(content))
+        {
+            Log.Error(Tag, "Clipboard content string cannot be empty!");
+            return false;
+        }
+
+        var clipboardService = (ClipboardManager?)Application.Context.GetSystemService(ClipboardService);
+
+        if (clipboardService is null)
+        {
+            ShowToast(Localization.clipboard_service_failed);
+
+            Log.Error(Tag, "Failed to get ClipboardService");
+            return false;
+        }
+
+        var clipData = ClipData.NewPlainText(ClipboardLabel, content);
         clipboardService.PrimaryClip = clipData;
 
-        Toast.MakeText(this, $"Copied {text}", ToastLength.Short)!.Show();
+        return true;
+    }
 
-        Finish();
+    /// <summary>
+    /// Show short toast with provided message content.
+    /// </summary>
+    /// <param name="message">Message to show on toast.</param>
+    private void ShowToast(string message)
+    {
+        var toast = Toast.MakeText(this, message, ToastLength.Short);
+
+        if (toast is null)
+            Log.Error(Tag, $"Failed create toast message with content '{message}'");
+
+        toast?.Show();
     }
 }
