e4211e81e16bf841624f778626c7ad99f3e223a7
Ezioleq <git@ezioleq.com>
2023-08-23 22:48:07 +0200

    Add switch to enable clip history
    - w/o actual implementation

--- ClipCopy/ViewModels/SettingsPageViewModel.cs
+++ ClipCopy/ViewModels/SettingsPageViewModel.cs
@@ -2,5 +2,26 @@ namespace ClipCopy.ViewModels;
 
 public class SettingsPageViewModel : ContentPage
 {
+    private bool _enableHistory;
 
+    public bool EnableHistory
+    {
+        get => _enableHistory;
+        set
+        {
+            _enableHistory = value;
+            OnPropertyChanged(nameof(EnableHistory));
+        }
+    }
+
+    protected override void OnPropertyChanged(string? propertyName = null)
+    {
+        if (propertyName == nameof(EnableHistory))
+            ToggleHistory(EnableHistory);
+    }
+
+    private void ToggleHistory(bool enabled)
+    {
+        // TODO: Set history preferences
+    }
 }
